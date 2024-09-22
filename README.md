1e16555bd3bdba16799e764814488565982616dc
Cyb3r-Jak3 <git@Cyberjake.xyz>
2024-09-22 16:05:53 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Docker Image Retagging
+
+This script is used to retag a docker image with a new tag. It is useful when you want to push the same image with a different tag to a different registry.
---
+++ build-matrix.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+
+def build_matrix() -> dict:
+    with open('images.json') as f:
+        matrix = json.load(f)
+    include = []
+    for source in matrix:
+        tags = matrix[source]['tags']
+        include.append({
+            "source": source,
+            "tags": tags
+        })
+    final_matrix = {
+        "source": "",
+        "tags": [],
+        "include": include
+    }
+    return final_matrix
+
+
+# Output needed
+# source: string
+# tags: list of strings
+
+
+if __name__ == '__main__':
+    output = build_matrix()
+    if os.environ.get('GITHUB_OUTPUT'):
+        with open(os.environ.get('GITHUB_OUTPUT'), 'a') as f:
+            print(f'matrix={json.dumps(output)}')
+    else:
+        print(json.dumps(output))
